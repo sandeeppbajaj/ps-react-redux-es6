15eb0adf322ca60294737658ee0a1f98b58c6f92
Sandeep Bajaj <sandeepp.bajaj@gmail.com>
2018-04-15 00:09:09 -0700

    Add boilerplate code for store, action and reducer

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import 'babel-polyfill';
 import React from 'react';
 import {render} from 'react-dom';
-import {Router, browserHistory} from 'react-router';
+import {browserHistory, Router} from 'react-router';
+import configureStore from './store/configureStore';
+import {Provider} from 'react-redux';
 import routes from './routes';
 import './styles/style.css'; //Webpack can import CSS files too!
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 
+const store = configureStore();
 render (
-  <Router history={browserHistory} routes={routes} />,
+  <Provider store={store}>
+    <Router history={browserHistory} routes={routes} />
+  </Provider>,
   document.getElementById('app')
 );
