7cb37c43f78e0a310e174eb5bcf5c44a5e83c75f
Sandeep Bajaj <sandeepp.bajaj@gmail.com>
2018-04-16 07:40:40 -0700

    Dispatch action on page load to get course list

--- src/index.js
+++ src/index.js
@@ -5,10 +5,15 @@ import {browserHistory, Router} from 'react-router';
 import configureStore from './store/configureStore';
 import {Provider} from 'react-redux';
 import routes from './routes';
+import {loadCourses} from "./actions/courseActions";
 import './styles/style.css'; //Webpack can import CSS files too!
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 
 const store = configureStore();
+
+// load courses
+store.dispatch(loadCourses());
+
 render (
   <Provider store={store}>
     <Router history={browserHistory} routes={routes} />
